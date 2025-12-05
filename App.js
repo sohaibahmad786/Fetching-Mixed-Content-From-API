37fb46d4b23249fb3cd8753826e556e419394a99
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2025-12-05 16:07:10 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import First_api from './API_Practice/First_api'
+
+const App = () => {
+  return (
+    <div>
+      <First_api />
+    </div>
+  )
+}
+
+export default App
+
+
+
